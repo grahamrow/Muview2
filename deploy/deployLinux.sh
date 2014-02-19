c280197616aa4020d1f5e04d778c616ddfffddac
Graham Rowlands <grahamrow@gmail.com>
2014-02-19 11:50:35 -0500

    Linux deployment scripts

---
+++ deploy/deployLinux.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+VERSION="2.0b3"
+NAME="Muview${VERSION}"
+
+if [ -e muview ]
+	then
+	rm muview
+fi
+if [ -e "${NAME}.tar.bz" ]
+	then
+	rm "${NAME}.tar.bz"
+fi
+# Kill old product
+if [ -d $NAME ]
+  then
+  echo "Removing $applicationName"
+  rm -r $NAME
+fi
+
+cd source
+make clean
+qmake
+cd ..
+make clean
+qmake
+make -j20
+
+FILES="muview README.md LICENSE"
+
+mkdir $NAME
+# Copy the install-only makefile to deploy directory
+cp deploy/MakefileInstall $NAME/Makefile
+# Copy the necessary resources, delete extraneous ones
+cp -R source/resources $NAME
+rm $NAME/resources/muview.icns $NAME/resources/splash*.png
+# Copy the rest of the files
+cp $FILES $NAME
+
+tar -cjvf "${NAME}.tar.bz" $NAME
+echo "Done deploying!"
