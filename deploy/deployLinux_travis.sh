231ee81dbe81498bd8695b27bbd71236d91a6e8c
Graham Rowlands <grahamrow@gmail.com>
2017-01-20 09:37:46 -0500

    Travis packaging appimage on linux

--- deploy/deployLinux_travis.sh
+++ deploy/deployLinux_travis.sh
@@ -9,12 +9,21 @@ FILES="muview README.md LICENSE"
 
 mkdir $NAME
 # Copy the install-only makefile to deploy directory
-cp deploy/MakefileInstall Muview/Makefile
+# cp deploy/MakefileInstall Muview/Makefile
 # Copy the necessary resources, delete extraneous ones
 cp -R source/resources $NAME
 rm $NAME/resources/muview.icns $NAME/resources/splash*.png
 # Copy the rest of the files
 cp $FILES $NAME
+
+# Package using linuxdeployqt
+echo "Creating AppImage bundle at $PWD/Muview.AppImage"
+echo "using qmake from $(which qmake)"
+echo "using linuxdeployqt from $(which linuxdeployqt)"
+
+cd $NAME
+linuxdeployqt muview -appimage -verbose=2
+cd ..
 
 tar -czvf "Muview.tar.gz" $NAME
 echo "Done deploying on Linux!"
