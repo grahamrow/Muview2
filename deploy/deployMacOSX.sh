4b5dfade8ab41eb20ddf34991ad2828899353ca4
Graham Rowlands <grahamrow@gmail.com>
2019-05-31 13:22:47 -0400

    More build fixes

--- deploy/deployMacOSX.sh
+++ deploy/deployMacOSX.sh
@@ -17,20 +17,38 @@ if [ -d $buildDir/$applicationName ]
   rm -r $buildDir/$applicationName
 fi
 
+# Fix version numbers
+if [[ $TRAVIS == true ]]
+  echo "Updating version numbers"
+  for name in source/aboutdialog.ui source/source.pro source/window.cpp
+  do
+    sed "s|2.2|${TRAVIS_BRANCH}|" $name > $name.temp
+    mv $name.temp $name
+  done
+fi
+
+# qmake version
+qmake_vers=$(qmake -v | tail -n1 | sed -n 's|.*\(/usr/.*\)/lib|\1|p')
+
 # This produces Muview 
 cd source
-make clean
 qmake -config release
+make clean
 cd ..
-make clean
 qmake -config release
+make clean
 make -j4
 
 echo "Creating Bundle at $PWD/Muview.app"
 echo "using qmake from $(which qmake)"
 echo "using macdeployqt from $(which macdeployqt)"
 
-macdeployqt $buildDir/$applicationName -verbose=3
+# Macdeployqt has known problems for brewed qt
+macdeployqt $buildDir/$applicationName -verbose=3 -always-overwrite
+curl -o fix_temp.py https://raw.githubusercontent.com/aurelien-rainone/macdeployqtfix/master/macdeployqtfix.py
+sed "s/deps = \[s.strip/deps = \[s.decode\(\).strip/" fix_temp.py > fix.py
+python macdeployqtfix.py Muview.app/Contents/MacOS/Muview $qmake_vers
+rm fix_temp.py fix.py
 
 if [ -e tmp.dmg ]
 	then
@@ -52,4 +70,3 @@ cp -R $applicationName staging
 echo "Creating DMG"
 hdiutil makehybrid -hfs -hfs-volume-name $title -hfs-openfolder staging staging -o tmp.dmg
 hdiutil convert -format UDZO tmp.dmg -o $finalDMGName
-
